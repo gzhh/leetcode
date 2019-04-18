1bc6d8c60b60a15e7d47f59904d8b4030390e12a
Zhonghuan Gao <zhonghuangao@gmail.com>
2019-04-18 23:24:53 +0800

    Update 160. Intersection of Two Linked Lists

--- algorithms/cpp/intersectionOfTwoLinkedLists/intersectionOfTwoLinkedLists.cpp
+++ algorithms/cpp/intersectionOfTwoLinkedLists/intersectionOfTwoLinkedLists.cpp
@@ -72,4 +72,22 @@ public:
         }
         return len;
     }
+
+    /**
+     * 思路二：
+     * 还有一种特别巧妙的方法，虽然题目中强调了链表中不存在环，但是我们可以用环的思想来做。
+     * 我们让两条链表分别从各自的开头开始往后遍历，当其中一条遍历到末尾时，我们跳到另一条链表的开头继续遍历。
+     * 两个指针最终会相等，而且只有两种情况，一种情况是在交点处相遇，另一种情况是在各自的末尾的空节点处相等。
+     * 为什么一定会相等呢，因为两个指针走过的路程相同，是两个链表的长度之和，所以一定会相等。
+     * 这个思路真的很巧妙，而且更重要的是代码写起来特别的简洁。
+     */
+    ListNode *getIntersectionNode(ListNode *headA, ListNode *headB) {
+        if (!headA || !headB) return NULL;
+        ListNode *a = headA, *b = headB;
+        while (a != b) {
+            a = a ? a->next : headB;
+            b = b ? b->next : headA;
+        }
+        return a;
+    }
 };
