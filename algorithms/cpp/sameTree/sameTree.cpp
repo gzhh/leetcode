d53179e663751e9fda5daccd689bbae1de594f0b
Zhonghuan Gao <zhonghuangao@gmail.com>
2019-05-12 13:49:58 +0800

    Submit 100. Same Tree

---
+++ algorithms/cpp/sameTree/sameTree.cpp
@@ -0,0 +1,52 @@
+// Source : https://leetcode.com/problems/same-tree/
+// Author : Zhonghuan Gao
+// Date   : 2019-05-12
+
+/********************************************************************************** 
+*
+Given two binary trees, write a function to check if they are the same or not.
+
+Two binary trees are considered the same if they are structurally identical and the nodes have the same value.
+
+Example 1:
+
+Input:     1         1
+          / \       / \
+         2   3     2   3
+
+        [1,2,3],   [1,2,3]
+
+Output: true
+Example 2:
+
+Input:     1         1
+          /           \
+         2             2
+
+        [1,2],     [1,null,2]
+
+Output: false
+Example 3:
+
+Input:     1         1
+          / \       / \
+         2   1     1   2
+
+        [1,2,1],   [1,1,2]
+
+Output: false
+*               
+**********************************************************************************/
+
+class Solution {
+public:
+    /**
+     * 解法一：
+     * 深搜递归
+     */
+    bool isSameTree(TreeNode* p, TreeNode* q) {
+        if (!p && !q) return true;
+        if ((!p && q) || (p && !q) || p->val != q->val) return false;
+        return isSameTree(p->left, q->left) && isSameTree(p->right, q->right);
+    }
+};
