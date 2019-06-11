ab24127d1a2e9bf5491e3e926578e0c2d2d06347
Zhonghuan Gao <zhonghuangao@gmail.com>
2019-06-12 00:13:01 +0800

    Update 86.Partision List

--- algorithms/cpp/partisionList/partisionList.go
+++ algorithms/cpp/partisionList/partisionList.go
@@ -41,4 +41,28 @@ public:
         }
         return dummy->next;
     }
+
+    /**
+     * 解法二：找出所有值小于x的节点，并将其移到一个新的链表，最后将新链表
+     * 的尾部连到原链表的头部
+     */
+    ListNode* partition(ListNode* head, int x) {
+        if (!head) return head;
+        ListNode *dummy = new ListNode(-1);
+        ListNode *newDummy = new ListNode(-1);
+        dummy->next = head;
+        ListNode *cur = dummy, *p = newDummy;
+        while (cur->next) {
+            if (cur->next->val < x) {
+                p->next = cur->next;
+                p = p->next;
+                cur->next = cur->next->next;
+                p->next = NULL;
+            } else {
+                cur = cur->next;
+            }
+        }
+        p->next = dummy->next;
+        return newDummy->next;
+    }
 };
